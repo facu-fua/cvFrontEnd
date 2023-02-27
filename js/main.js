2656df95aaf28b01ad8b1e78697e11e5094480d3
facu-fua <facu_232@hotmail.com>
2023-02-27 20:14:11 -0300

    First commit, cv proyect

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+//Cambiar tema del sitio
+let themebutton = document.getElementById("themeButton");
+let theme = document.getElementsByTagName("body")[0];
+
+themebutton.onclick = () =>{
+    if (theme.classList.contains("light")){
+        theme.classList.remove("light");
+        theme.classList.add("dark");
+    }else{
+        theme.classList.remove("dark");
+        theme.classList.add("light");
+    }
+};
+
+let emailForm = document.querySelector(".emailForm");
+emailForm.onsubmit = (e) =>{
+    e.preventDefault();
+    let message = {
+        mail: email.value,
+        text: emailText.value
+    }
+    let status = document.querySelector(".status");
+    status.innerHTML = `<h3>Mensaje enviado!`;
+    console.log(message)
+}
